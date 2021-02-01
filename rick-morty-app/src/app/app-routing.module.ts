5fa4d6982db38c2785e4856544b116346b0a92aa
Gamaliel Leon <e-gentrambasaguas@neoris.com>
2021-01-31 20:22:13 -0600

    Se agregaron constantes para las rutas y se generaron las paginas de login, register y profile

--- rick-morty-app/src/app/app-routing.module.ts
+++ rick-morty-app/src/app/app-routing.module.ts
@@ -5,7 +5,19 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
   }
+
 ];
 @NgModule({
   imports: [
---
+++ rick-morty-app/src/app/constants/paths.ts
@@ -0,0 +1,3 @@
+export const LOGIN: string = 'login';
+export const REGISTER: string = 'register';
+export const PROFILE: string = 'profile';
