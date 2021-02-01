c87061cb470ba21535ea1d2f54e60cf68ac00a49
Gamaliel Leon <e-gentrambasaguas@neoris.com>
2021-02-01 17:32:17 -0600

    Creación de formulario de registro. Se añadieron constantes para los patrones de validación y las rutas de navegación

---
+++ rick-morty-app/src/app/constants/registerErrors.ts
@@ -0,0 +1,5 @@
+export const registerErrors: any = {
+    firstName: {
+        type: ''
+    }
+};
--- rick-morty-app/src/app/pages/register/register.page.ts
+++ rick-morty-app/src/app/pages/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EMAIL_PATTERN, NAME_PATTERN, PASSWORD_PATTERN, PHONE_PATTERN } from 'src/app/constants/patterns';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  registerForm: FormGroup;
+  private passwordsMatched: boolean = true;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.pattern(NAME_PATTERN)]],
+      lastName: ['', [Validators.required, Validators.pattern(NAME_PATTERN)]],
+      email: ['', [Validators.required, Validators.pattern(EMAIL_PATTERN)]],
+      phone: ['', [Validators.required, Validators.pattern(PHONE_PATTERN)]],
+      password: ['', [Validators.required, Validators.pattern(PASSWORD_PATTERN)]],
+      passwordConfirm: ['', [Validators.required, Validators.pattern(PASSWORD_PATTERN)]]
+    });
   }
 
+  ngOnInit() { }
+
+  /********** METHODS **********/
+  onSubmitRegister(): void {
+    console.log('submit!');
+  }
+  checkPasswordConfirm(): void{
+    const formControls = this.registerForm.controls;
+    this.setPasswordsMatched(formControls.password.value === formControls.passwordConfirm.value);
+    /* if (formControls.password.value.length < 8) {
+      this.setErrorPassword(ERROR_PASSWORD);
+    }
+    else if (formControls.password.invalid) {
+      this.setErrorPassword(ERROR_FORMAT_PASSWORD);
+    } */
+  }
+  isFieldInvalid(fieldName: string): boolean{
+    const field = this.registerForm.controls[fieldName];
+    return field.invalid && field.touched;
+  }
+  /********** GETTERS **********/
+  getPasswordsMatched = (): boolean => this.passwordsMatched;
+  /********** SETTERS **********/
+  setPasswordsMatched(passwordsMatched: boolean): void { this.passwordsMatched = passwordsMatched; }
 }
