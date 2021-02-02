941637e08c6a85030ae016aef56f95e4ede81977
e-tfuentesm <74672276+e-tfuentesm@users.noreply.github.com>
2021-02-01 20:25:09 -0600

    modificación de modulos, solo copiamos y editamos

--- rick-morty-app/src/app/pages/tabs/tabs-routing.module.ts
+++ rick-morty-app/src/app/pages/tabs/tabs-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { REGISTER, PROFILE, CHARACTERS } from 'src/app/constants/paths';
+import { PROFILE, CHARACTERS } from 'src/app/constants/paths';
 import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
