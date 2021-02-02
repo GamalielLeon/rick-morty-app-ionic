74089438619797e55c8b47c2517cf60cefc60b71
Gamaliel Leon <e-gentrambasaguas@neoris.com>
2021-02-02 17:58:30 -0600

    Revert "Commit para actualizar master"
    This reverts commit 0772efff1b7358b8a1db60b8d58a4eb067f9e773.

--- rick-morty-api/src/routes/authJwt.js
+++ rick-morty-api/src/routes/authJwt.js
@@ -2,7 +2,7 @@ const { name, secret, algorithm, expiresIn, httpOnly } = require('../constants/j
 const { Router } = require('express');
 const jwt = require('jsonwebtoken');
 const router = Router();
-//
+
 router.post('/', (req, res) => {
     const token = jwt.sign({ email: req.body.email }, secret, { algorithm, expiresIn });
     res.cookie(name, token, { maxAge: expiresIn * 1000, httpOnly });
